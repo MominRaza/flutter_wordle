ee5d94e93fda7fbe3a35aba8d48ffa28fe217f9c
Momin Ahmad <mominr082@gmail.com>
2023-08-15 20:28:56 +0530

    added emoji chart

--- lib/main.dart
+++ lib/main.dart
@@ -247,19 +247,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                               showDialog(
                                                 context: context,
                                                 builder: (context) =>
-                                                    AlertDialog(
-                                                  title: const Text('You win'),
-                                                  content: const Text(
-                                                      '''ApnaPan Worldle 001 3/6
-                                                          🟩🟨⬛'''),
-                                                  actions: [
-                                                    FilledButton.tonal(
-                                                      onPressed: () {},
-                                                      child:
-                                                          const Text('Share'),
-                                                    ),
-                                                  ],
-                                                ),
+                                                    WinDialog(card),
                                               );
                                             }
 
@@ -353,6 +341,42 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class WinDialog extends StatelessWidget {
+  const WinDialog(
+    this.card, {
+    super.key,
+  });
+
+  final List<List<Map<dynamic, dynamic>>> card;
+
+  @override
+  Widget build(BuildContext context) {
+    String emoji = '';
+    for (var e in card) {
+      for (var e in e) {
+        e['match'] == 'MATCHED'
+            ? emoji += '🟩'
+            : e['match'] == 'PARTIAL'
+                ? emoji += '🟨'
+                : e['match'] == 'UNMATCHED'
+                    ? emoji += '⬛'
+                    : '';
+      }
+      emoji += '\n';
+    }
+    return AlertDialog(
+      title: const Text('You win'),
+      content: Text('Flutter Worldle\n$emoji'.trim()),
+      actions: [
+        FilledButton.tonal(
+          onPressed: () {},
+          child: const Text('Share'),
+        ),
+      ],
     );
   }
 }
