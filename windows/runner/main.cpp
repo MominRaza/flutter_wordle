cae8cb3981bd6b84657fa21b8c74b7eb2b98f0db
Momin Ahmad <mominr082@gmail.com>
2023-08-17 19:07:24 +0530

    Flutter Wordle

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(480, 800);
-  if (!window.Create(L"flutter_wordle", origin, size)) {
+  if (!window.Create(L"Flutter Wordle", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
